8a312776461891eb6a00fef700da05f83348db31
vishakha <vishakhadhonde16@gmail.com>
2025-02-25 05:31:30 +0000

    cm4

--- test/test.js
+++ test/test.js
@@ -1,19 +1,15 @@
-              var request = require('supertest');
-              var app = require('../index.js');
-               
-              describe('GET /', function() {
-                it('respond with 404 page not found', function(done) {
-                  request(app)
-                    .get('/nonexistentpage')
-                    .expect(404)
-                    .end(function(err, res) {
-                      if (err) {
-                        // If there's an error, log it and pass it to the done callback
-                        console.error(err);
-                        return done(err);
-                      }
-                      // If everything is fine, invoke the done callback
-                      done();
-                    });
-                });
-              });
+var request = require('supertest');
+var app = require('../index.js');
+
+describe('GET /', function () {
+  after(function () {
+    process.exit(0); // Ensure Mocha exits after tests
+  });
+
+  it('respond with 404 page not found', function (done) {
+    request(app)
+      .get('/nonexistentpage')
+      .expect(404, done);
+  });
+});
+
